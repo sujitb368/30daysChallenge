5350e1525ff0b3c9ff54911591caa830d9a45581
sujitb368 <42778388+sujitb368@users.noreply.github.com>
2023-10-02 09:41:48 +0530

    update console

--- Day28/day28.js
+++ Day28/day28.js
@@ -3,12 +3,14 @@
 
 const rotated = (arr, k) => {
   try {
-    const len = arr.length;
-
+    //extract last k number elements from array
     const kArr = arr.splice(-k);
     return [...kArr, ...arr];
   } catch (error) {
     console.log("Error", error);
   }
 };
-console.log(rotated([1, 2, 3, 4, 5], 1));
+console.log(rotated([1, 2, 3, 4, 5], 1)); //[ 5, 1, 2, 3, 4 ]
+console.log(rotated([5, 2, 3, 4, 1], 1)); //[ 1, 5, 2, 3, 4 ]
+console.log(rotated([1, 2, 3, 4, 5], 2)); //[ 4, 5, 1, 2, 3 ]
+console.log(rotated([5, 2, 3, 4, 1], 2)); //[ 4, 1, 5, 2, 3 ]
